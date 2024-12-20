a2e14d6090c427db93ed77682aa141df02c3d2d3
jbaxax <walterjave92@gmail.com>
2024-12-19 22:58:44 -0500

    2 first

--- components/featured-products.tsx
+++ components/featured-products.tsx
@@ -1,15 +1,17 @@
 "use client"
 import { useGetFeaturedProducts } from "@/api/useGetFeaturedProducts";
 import { RespondeType } from "@/types/response";
-import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
 import SkeletonSchema from "./skeletonSchema";
 import { ProductType } from "@/types/product";
 import { Card, CardContent } from "./ui/card";
 import { Expand, ShoppingCart } from "lucide-react";
+import IconButton from "./icon-button";
+import { useRouter } from "next/navigation";
 
 const FeaturedProducts = () => {
   const { loading, result }: RespondeType = useGetFeaturedProducts();
-
+  const router = useRouter()
   console.log(result);
 
   return (
@@ -37,25 +39,44 @@ const FeaturedProducts = () => {
                   <div className="p-1">
                     <Card className="py-4 border border-gray-200 shadow-none">
                       <CardContent className="relative flex items-center justify-center px-6 py-2">
-                        
-                          <img
-                            src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images[0].url}`}
-                            alt={"Imagen destacada"}
-                          />
-                        
+
+                        <img
+                          src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images[0].url}`}
+                          alt={"Imagen destacada"}
+                        />
+
                         <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
                           <div className="flex justify-center gap-x-6">
-                            <Expand />
-                            <ShoppingCart />
+                            <IconButton onClick={() => router.push(`product/${slug}`)}
+                              icon={<Expand size={20} />}
+                              className="text-gray-600"
+                            />
+
+                            <IconButton onClick={() => router.push(`product/${slug}`)}
+                              icon={<ShoppingCart size={20}/>}
+                              className="text-gray-600"
+                            />
+
+                            
+
                           </div>
                         </div>
                       </CardContent>
+                      <div className="flex justify-between gap-4 px-8">
+                        <h3 className="text-lg font-bold">{productName}</h3>
+                        <div className="flex items-center justify-between gap-3">
+                          <p className="px-2 py-1 text-white bg-black rounded-full dark:bg-white dark:text-black w-fit">{taste}</p>
+                          <p className="px-2 py-1 text-white bg-yellow-900 rounded-full w-fit">{origin}</p>
+                        </div>
+                      </div>
                     </Card>
                   </div>
                 </CarouselItem>
               );
             })}
         </CarouselContent>
+        <CarouselPrevious/>
+        <CarouselNext className="hidden sm:flex" />
       </Carousel>
     </div>
   );
